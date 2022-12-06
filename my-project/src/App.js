ff5fb36875ba913e68eb8c22d3883c1b713156c9
aTreeFrog <shyonbabazadeh@gmail.com>
2022-12-05 21:55:37 -0800

    following tests so far

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { contractAbi, contractAddress } from './utils/constants';
+
+const web3 = new Web3("ws://localhost:8545")
+const greeterContract = new web3.eth.Contract(contractAbi, contractAddress);
 
 function App() {
   return (
---
+++ my-project/src/utils/constants.js
@@ -0,0 +1,4 @@
+import Lock from './Lock.json';
+
+export const contractAbi = Lock.abi;
+export const contractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
