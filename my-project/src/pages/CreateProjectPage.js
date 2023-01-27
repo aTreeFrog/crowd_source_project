948ffacc8e165c29641830d496a047e664cd302c
aTreeFrog <shyonbabazadeh@gmail.com>
2023-01-27 15:06:26 -0800

    project creation area

--- my-project/src/pages/CreateProjectPage.js
+++ my-project/src/pages/CreateProjectPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { TextBox } from './PageElements'
+import { TextBox, TextArea } from './PageElements'
 
 const CreateProjectPage = () => {
     const [greetings, setGreetings] = useState("");
@@ -8,16 +8,55 @@ const CreateProjectPage = () => {
     const [description, setDescription] = useState("");
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'Center',
-                alignItems: 'Center',
-                //height: '100vh'
-            }}
-        >
-            <TextBox defaultValue="Type Project Name" type="text" value={greetings}
-                onChange={(e) => setGreetings(e.target.value)} />
+        <div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'Center',
+                    alignItems: 'Center',
+                    //height: '100vh'
+                }}>
+                Project Name
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'Center',
+                    alignItems: 'Center',
+                    //height: '100vh'
+                }}
+            >
+
+                <TextBox style={{
+                    height: '12px',
+                    width: '300px',
+                }} type="text" value={name}
+                    onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'Center',
+                    alignItems: 'Center',
+                    //height: '100vh'
+                }}>
+                Project Description
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'Center',
+                    alignItems: 'Center',
+                    //height: '100vh'
+                }}
+            >
+
+                <TextArea style={{
+                    height: '200px',
+                    width: '300px',
+                }} type="text" value={description}
+                    onChange={(e) => setDescription(e.target.value)} />
+            </div>
         </div>
     );
 };
--- my-project/src/pages/PageElements.js
+++ my-project/src/pages/PageElements.js
@@ -4,7 +4,35 @@ export const TextBox = styled.input`
 padding: 0.5em;
 margin: 0.5em;
 color: ${props => props.inputColor || "black"};
-background: papayawhip;
+background: #E5F4FC;
 border: none;
 border - radius: 3px;
+`;
+
+export const TextArea = styled.textarea`
+padding: 0.5em;
+margin: 0.5em;
+color: ${props => props.inputColor || "black"};
+background: #E5F4FC;
+border: none;
+border - radius: 3px;
+
+
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextRow = styled.div`
+  text-align: left;
+
+`;
+
+export const TextBoxRow = styled.div`
+  align-self: left;
+
 `;
