cddeed3819f373fc4401d2a60efcbafc49754953
aTreeFrog <shyonbabazadeh@gmail.com>
2023-01-28 20:37:41 -0800

    create project page good with opacity button

--- my-project/src/pages/CreateProjectPage.js
+++ my-project/src/pages/CreateProjectPage.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import { useState } from 'react';
-import { TextBox, TextArea } from './PageElements'
+import { TextBox, TextArea, ImageUploader, MyTagContainer, MyTags, TagInputs, TagButton, SubmitBtn } from './PageElements'
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import IconButton from '@material-ui/core/IconButton';
+import { TermsContainer, CheckboxContainer } from './PageElements';
 
 const CreateProjectPage = () => {
-    const [greetings, setGreetings] = useState("");
+
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [inputTag, setInputTag] = useState('');
+    const [tags, setTags] = useState([]);
+    const [isKeyReleased, setIsKeyReleased] = useState(false)
+    const [isTermsChecked, setIsTermsChecked] = useState(false);
+
+    const onKeyDown = (e) => {
+        const { key } = e;
+        let trimmedInput = inputTag.trim();
+
+        if (!trimmedInput.startsWith("#")) {
+            console.log("Hi im here");
+            trimmedInput = '#' + trimmedInput;
+        }
+
+        if (key === "Enter" && trimmedInput.length && !tags.includes(trimmedInput)) {
+            e.preventDefault();
+            setTags(prevState => [...prevState, trimmedInput]);
+            setInputTag('');
+        }
+
+        if (key === "Backspace" && !inputTag.length && tags.length) {
+            e.preventDefault();
+            const tagsCopy = [...tags];
+            const poppedTag = tagsCopy.pop();
+
+            setTags(tagsCopy);
+            setInputTag(poppedTag);
+        }
+
+        setIsKeyReleased(false);
+    };
+
+    const onKeyUp = () => {
+        setIsKeyReleased(true);
+    }
+
+    const deleteTag = (index) => {
+        setTags(prevState => prevState.filter((tag, i) => i !== index))
+    }
+
 
     return (
         <div>
@@ -57,7 +101,105 @@ const CreateProjectPage = () => {
                 }} type="text" value={description}
                     onChange={(e) => setDescription(e.target.value)} />
             </div>
-        </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'Center',
+                alignItems: 'Center',
+                //height: '100vh'
+            }}>
+                Logo
+            </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'Center',
+                alignItems: 'Center',
+                //height: '100vh'
+            }}>
+                {selectedImage && (
+                    <div>
+                        <img alt="not found" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+                        <br />
+                        <button onClick={() => setSelectedImage(null)}>Remove</button>
+                    </div>
+                )}
+                <br />
+                <br />
+                <ImageUploader
+                    type="file"
+                    name="myImage"
+                    id="load-image-icon"
+                    style={{ display: 'none' }}
+                    onChange={(event) => {
+                        console.log(event.target.files[0]);
+                        setSelectedImage(event.target.files[0]);
+                    }}
+                />
+                <label htmlFor="load-image-icon">
+                    <IconButton color="primary" aria-label="upload picture"
+                        component="span">
+                        <PhotoCamera />
+                    </IconButton>
+                </label>
+            </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'Center',
+                alignItems: 'Center',
+                //height: '100vh'
+            }}>
+                Categories
+            </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'Center',
+                alignItems: 'Center',
+                //height: '100vh'
+            }}>
+                <MyTagContainer
+                    style={{
+                        minheight: '30px',
+                        width: '500px',
+                    }}>
+                    {tags.map((tag, index) => (
+                        <MyTags>
+                            {tag}
+                            <TagButton onClick={() => deleteTag(index)}>x</TagButton>
+                        </MyTags>))}
+                    <TagInputs
+                        value={inputTag}
+                        placeholder="#EnterTagsHere"
+                        onKeyDown={onKeyDown}
+                        onKeyUp={onKeyUp}
+                        onChange={(e) => setInputTag(e.target.value)} />
+
+                </MyTagContainer>
+            </div>
+            <br />
+            <br />
+            <TermsContainer>
+                <label htmlFor="terms-and-conditions">
+                    <CheckboxContainer>
+                        <input
+                            type="checkbox"
+                            id="terms-and-conditions"
+                            checked={isTermsChecked}
+                            onChange={() => setIsTermsChecked(!isTermsChecked)}
+                        />
+                        I agree to the terms and conditions in this insane project they we are all working on here today.
+                    </CheckboxContainer>
+                </label>
+            </TermsContainer>
+            <br />
+            <br />
+            <div style={{
+                display: 'flex',
+                justifyContent: 'Center',
+                alignItems: 'Center',
+                //height: '100vh'
+            }}>
+                <SubmitBtn disabled={!isTermsChecked}><span>Submit Project</span></SubmitBtn>
+            </div>
+        </div >
     );
 };
 
--- my-project/src/pages/PageElements.js
+++ my-project/src/pages/PageElements.js
@@ -5,8 +5,8 @@ padding: 0.5em;
 margin: 0.5em;
 color: ${props => props.inputColor || "black"};
 background: #E5F4FC;
-border: none;
-border - radius: 3px;
+border: 1px solid #E5F4FC;
+border-radius: 5px;
 `;
 
 export const TextArea = styled.textarea`
@@ -14,10 +14,11 @@ padding: 0.5em;
 margin: 0.5em;
 color: ${props => props.inputColor || "black"};
 background: #E5F4FC;
-border: none;
-border - radius: 3px;
+border: 1px solid #E5F4FC;
+border-radius: 5px;
+`;
 
-
+export const ImageUploader = styled.input`
 `;
 
 export const Container = styled.div`
@@ -35,4 +36,103 @@ export const TextRow = styled.div`
 export const TextBoxRow = styled.div`
   align-self: left;
 
+`;
+
+export const MyTagContainer = styled.div`
+display: flex;
+align-items: center;
+overflow: auto;
+justify-content: center;
+padding: 0.5em;
+margin: 0.5em;
+color: ${props => props.inputColor || "black"};
+background: #E5F4FC;
+border: 1px solid #E5F4FC;
+border-radius: 5px;
+`;
+
+export const MyTermsInput = styled.input`
+overflow: auto;
+border: none;
+border-radius: 5px;
+padding: 5px;
+padding-left: 5px;
+color: ${props => props.inputColor || "black"};
+background: none;
+`;
+
+export const TermsContainer = styled.div`
+display:inline-block;
+background-color: #FAF7F7;
+`;
+
+export const CheckboxContainer = styled.div`
+display:inline-block;
+max-width:400px;
+word-wrap: break-word;
+white-space: pre-wrap;
+margin-Left: 45px;
+`;
+
+export const MyTags = styled.div`
+display: flex;
+align-items: center;
+margin: 7px 0;
+margin-right: 10px;
+padding: 0 10px;
+padding-right: 5px;
+border: 1px solid orange;
+border-radius: 5px;
+background-color: orange;
+white-space: nowrap;
+color: white;
+`;
+
+export const TagInputs = styled.input`
+width: 100%;
+min-width: 50%;
+overflow: auto;
+border: none;
+border-radius: 5px;
+padding: 14px;
+padding-left: 14px;
+color: ${props => props.inputColor || "black"};
+background: #E5F4FC;
+border: none;
+`;
+
+export const TagButton = styled.button`
+display: flex;
+padding: 6px;
+border: none;
+background-color: unset;
+cursor: pointer;
+color: white;
+`;
+
+export const SubmitBtn = styled.button`
+border-radius: 4px;
+background: #36A3CF;  //#808080 was original
+padding: 10px 22px;
+color: white; //#000000 was original
+font-weight: bold;
+outline: none;
+border: none;
+vertical-align: middle;
+height: 35px;
+margin-top: 10px;
+align-items: center;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+/* Second Nav */
+margin-left: 24px;
+&:hover {
+	transition: all 0.2s ease-in-out;
+	background: orange;
+	color: #808080;
+}
+:disabled {
+    opacity: 0.4;
+  }
 `;
